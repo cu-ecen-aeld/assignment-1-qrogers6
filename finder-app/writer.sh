b08da420bcc0b72d571d91bfbfa7c88743b2f2c5
Quincy Rogers <quro7799@colorado.edu>
2024-01-19 08:58:48 -0800

    Assignment 1 complete.

---
+++ finder-app/writer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Accept 2 runtime arguments
+if [ $# -ne 2 ]; then
+  echo "Expected number of args is 2, not [$#]."
+  exit 1
+fi
+
+# Full path to to a file (including filename)
+writefile=$1
+# test string which will be written to file 
+writestr=$2
+
+# Specified file name
+fileName=$(basename $writefile)
+# Specified directory name
+dirName=$(dirname $writefile)
+
+# Does current directory exist? Create if it doesn't
+if [ ! -d $dirName ]; then
+  mkdir -p $dirName
+
+  if [ $? -ne 0 ]; then
+    echo "Failed to create directory [$dirName]."
+    exit 1
+  fi
+fi
+
+# Write string to file
+echo $writestr > $writefile
+
+exit 0
+
